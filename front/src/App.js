98b481c9d1054822052cc338abfd06d099c2a491
Aymeric weinbach <jean_sive@msn.com>
2020-06-18 18:24:24 +0000

    new upload

--- front/src/App.js
+++ front/src/App.js
@@ -46,57 +46,72 @@ function App() {
                         'Accept': 'application/json',
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify({container:'img'})
+                    body: JSON.stringify({container:'img',blobName:file.name,permissions:'awl'})
                   }
-                  const response = await fetch(`/api/sas/`,config);
-                  const json = await response.json();
-                  console.log(json);
+                  console.log(file)
+                  const response = await fetch(`http://localhost:7072/api/sas/`,config);
+                  const sas = await response.json();
+                  console.log(sas);
+                  // const configUpload  = {
+                  //   method: 'PUT',
+                  //   headers:{
+                  //     'Content-Type': file.type,
+                  //     'x-ms-blob-type':'BlockBlob'
+                  //   },
+                  //   body:file
+                  // }
+                  // const uploadSuccess = await fetch(sas.uri,configUpload);
+                  // const isSuccess  = await uploadSuccess;
+                  // console.log(isSuccess);
+
+
                   //this.setState({ data: json });
                    // fieldName is the name of the input field
                     // file is the actual file object to send
                     // const formData = new FormData();
                     // formData.append(fieldName, file, file.name);
 
-                    // const request = new XMLHttpRequest();
+                  const request = new XMLHttpRequest();
                     // const url =process.env.REACT_APP_UPLOAD //'https://wgtempupload.azurewebsites.net/api/fpupload/'
                     // const insta = document.getElementById("insta").value//document.getElementById("insta").value=="" ? document.getElementById("insta").value :document.getElementById("insta").defaultvalue
                     // const tosend= url+insta+"-"+file.name.toLowerCase();
                     // const auth = new Auth();
                     // const uid=auth.currentUser().id;//'260717ac-9153-4d35-9683-d1459be8b35d';
-                    // request.open('POST',tosend );
-                    // request.setRequestHeader('X-UserId',uid);
+                  request.open('PUT',sas.uri );
+                  request.setRequestHeader('Content-Type',file.type);
+                  request.setRequestHeader('x-ms-blob-type','BlockBlob');
                     // // Should call the progress method to update the progress to 100% before calling load
                     // // Setting computable to false switches the loading indicator to infinite mode
-                    // request.upload.onprogress = (e) => {
-                    //     progress(e.lengthComputable, e.loaded, e.total);
-                    // };
+                  request.upload.onprogress = (e) => {
+                      progress(e.lengthComputable, e.loaded, e.total);
+                  };
 
                     // // Should call the load method when done and pass the returned server file id
                     // // this server file id is then used later on when reverting or restoring a file
                     // // so your server knows which file to return without exposing that info to the client
-                    // request.onload = function() {
-                    //     if (request.status >= 200 && request.status < 300) {
-                    //         // the load method accepts either a string (id) or an object
-                    //         load(request.responseText);
-                    //     }
-                    //     else {
-                    //         // Can call the error method if something is wrong, should exit after
-                    //         error('oh no');
-                    //     }
-                    // };
+                  request.onload = function() {
+                      if (request.status >= 200 && request.status < 300) {
+                          // the load method accepts either a string (id) or an object
+                          load(request.responseText);
+                      }
+                      else {
+                          // Can call the error method if something is wrong, should exit after
+                          error('oh no');
+                      }
+                  };
 
-                    // request.send(formData);
+                  request.send(file);
                     
                     // // Should expose an abort method so the request can be cancelled
-                    // return {
-                    //     abort: () => {
-                    //         // This function is entered if the user has tapped the cancel button
-                    //         request.abort();
+                  return {
+                      abort: () => {
+                          // This function is entered if the user has tapped the cancel button
+                          request.abort();
 
-                    //         // Let FilePond know the request has been cancelled
-                    //         abort();
-                    //     }
-                    // };
+                          // Let FilePond know the request has been cancelled
+                          abort();
+                      }
+                  };
         
                }
             }
