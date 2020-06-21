a5f95fb7e89fb347aebfdf6b7fd77804413fbd5b
Aymeric weinbach <jean_sive@msn.com>
2020-06-21 16:07:56 +0000

    gallery

--- front/src/gallery.js
+++ front/src/gallery.js
@@ -2,24 +2,30 @@ import React from 'react';
 
 import { LazyLoadImage, trackWindowScroll }
   from 'react-lazy-load-image-component';
-
+import PhotoGallery from "react-photo-gallery";
 import 'react-lazy-load-image-component/src/effects/blur.css';
+
+
+
+
 const Gallery = ({ images, scrollPosition }) => (
   <div>
-    {images.map((image) =>
-      <LazyLoadImage
-        key={image.key}
-        alt={image.alt}
-        height={image.height}
-        effect="blur"
-        placeholderSrc={ image.lowResSrc }
-        // Make sure to pass down the scrollPosition,
-        // this will be used by the component to know
-        // whether it must track the scroll position or not
-        scrollPosition={scrollPosition}
-        src={image.src}
-        width={image.width} />
-    )}
+       <PhotoGallery photos={images} renderImage={
+          ({ index, left, top, key, photo }) => (
+            <LazyLoadImage
+              key={photo.key}
+              alt={photo.alt}
+              height={photo.height}
+              effect="blur"
+              placeholderSrc={ photo.lowResSrc }
+              // Make sure to pass down the scrollPosition,
+              // this will be used by the component to know
+              // whether it must track the scroll position or not
+              scrollPosition={scrollPosition}
+              src={photo.src}
+              width={photo.width} />
+              )
+       } />
   </div>
 );
 // Wrap Gallery with trackWindowScroll HOC so it receives
